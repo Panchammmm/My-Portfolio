766bd15d975b0167bcc8318cc4154b80cd8c6650
Panchammmm <sardarpancham510510@gmail.com>
2024-05-24 15:25:16 +0530

    home-UI

--- src/Components/NavBar/Nav.jsx
+++ src/Components/NavBar/Nav.jsx
@@ -2,87 +2,68 @@ import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
 import { MoonStar, SunDim } from 'lucide-react';
 
-const menuItems = [
-    {
-        name: 'Home',
-        href: '#',
-    },
-    {
-        name: 'About',
-        href: '#',
-    },
-    {
-        name: 'Project',
-        href: '#',
-    },
-    {
-        name: 'Contact',
-        href: '#',
-    },
-]
-
 export default function Nav() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isJapanese, setIsJapanese] = useState(true);
+    const [isLightMode, setIsLightMode] = useState(true);
 
     const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen)
-    }
-
-    const [isJapanese, setIsJapanese] = useState(true);
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     const toggleLanguage = () => {
         setIsJapanese(!isJapanese);
-    }
-
-    const [isLightMode, setIsLightMode] = useState(true);
+    };
 
     const toggleMode = () => {
         setIsLightMode(!isLightMode);
-    }
+    };
 
     return (
         <div className="relative shadow-2xl sm:w-[55%] w-[85%] bg-black rounded-[30px] mx-auto sm:mt-14 mt-9">
             <div className="mx-auto flex max-w-7xl items-center justify-between px-7 py-3 sm:px-6 lg:px-8">
 
                 <div className="inline-flex items-center space-x-2">
-                    <span>
-                        <>
-                            {/* any icon */}
-                        </>
-                    </span>
-
-                    <div className="flex items-center cursor-pointer text-white text-2xl ">
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">P</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">o</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">r</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">t</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">f</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">o</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">l</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">i</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">o</span>
-                        <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">.</span>
+                    <div className="flex items-center cursor-pointer text-white text-2xl animate-leftSideAni">
+                        {["P", "o", "r", "t", "f", "o", "l", "i", "o", "."].map((char, index) => (
+                            <span
+                                key={index}
+                                className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2"
+                            >
+                                {char}
+                            </span>
+                        ))}
                     </div>
                 </div>
 
-                {/* navbar */}
+                {/* Navbar */}
                 <div className="hidden xl:block">
                     <ul className="inline-flex space-x-10">
-                        {menuItems.map((item) => (
-                            <li key={item.name}>
-                                <a
-                                    href={item.href}
-                                    className="text-[15px] text-base text-white hover:text-pink-300"
-                                >
-                                    {item.name}
-                                </a>
-                            </li>
-                        ))}
+                        <li>
+                            <a href="#" className="animate-navani delay-100 active:text-violet-500 hover:text-violet-300 text-[15px] text-base text-white">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="animate-navani delay-200 active:text-violet-500 hover:text-violet-300 text-[15px] text-base text-white">
+                                About
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="animate-navani delay-300 active:text-violet-500 hover:text-violet-300 text-[15px] text-base text-white">
+                                Project
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" className="animate-navani delay-400 active:text-violet-500 hover:text-violet-300 text-[15px] text-base text-white">
+                                Contact
+                            </a>
+                        </li>
                     </ul>
                 </div>
 
                 <div className="hidden xl:flex items-center justify-center">
-                    <button className="cursor-pointer text-white px-4" onClick={toggleMode}>
+                    <button className="animate-rightSideAni cursor-pointer text-white px-4" onClick={toggleMode}>
                         {isLightMode ? (
                             <SunDim className="text-white" />
                         ) : (
@@ -93,13 +74,13 @@ export default function Nav() {
                     <button
                         type="button"
                         onClick={toggleLanguage}
-                        className="shadow-lg shadow-orange-500/40 rounded-[30px] px-4 py-2 text-sm font-medium tracking-wider text-rose-50 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-75"
+                        className="animate-rightSideAni delay-100 shadow-lg shadow-orange-500/40 rounded-[30px] px-4 py-2 text-sm font-medium tracking-wider text-rose-50 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-75"
                     >
                         {isJapanese ? '日本語' : 'ENG'}
                     </button>
                 </div>
 
-                {/* navMenu */}
+                {/* NavMenu */}
                 <div className="xl:hidden">
                     <Menu onClick={toggleMenu} className="h-6 w-6 cursor-pointer text-white" />
                 </div>
@@ -108,25 +89,16 @@ export default function Nav() {
                         <div className="divide-y-2 divide-gray-50 rounded-lg bg-black shadow-lg ring-1 ring-black ring-opacity-5">
                             <div className="px-5 pb-6 pt-5">
                                 <div className="flex items-center justify-between">
-
                                     <div className="inline-flex items-center space-x-2">
-                                        <span>
-                                            <>
-                                                {/* any icon */}
-                                            </>
-                                        </span>
-
                                         <div className="flex items-center cursor-pointer text-white text-2xl">
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">P</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">o</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">r</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">t</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">f</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">o</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">l</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">i</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">o</span>
-                                            <span className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2">.</span>
+                                            {["P", "o", "r", "t", "f", "o", "l", "i", "o", "."].map((char, index) => (
+                                                <span
+                                                    key={index}
+                                                    className="transition-all duration-300 ease-in-out transform hover:text-pink-500 hover:-translate-y-2"
+                                                >
+                                                    {char}
+                                                </span>
+                                            ))}
                                         </div>
                                     </div>
 
@@ -134,7 +106,7 @@ export default function Nav() {
                                         <button
                                             type="button"
                                             onClick={toggleMenu}
-                                            className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                                            className="animate-rightSideAni inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                                         >
                                             <span className="sr-only">Close menu</span>
                                             <X className="h-6 w-6" aria-hidden="true" />
@@ -144,22 +116,23 @@ export default function Nav() {
 
                                 <div className="mt-6">
                                     <nav className="grid gap-y-4">
-                                        {menuItems.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className="-m-3 flex items-center rounded-md p-3 text-sm font-base"
-                                            >
-                                                <span className="ml-3 text-base font-medium text-white">
-                                                    {item.name}
-                                                </span>
-                                            </a>
-                                        ))}
+                                        <a href="#" className="animate-navani delay-100 -m-3 flex items-center rounded-md p-3 text-sm font-base active:bg-violet-700">
+                                            <span className="ml-3 text-base font-medium text-white">Home</span>
+                                        </a>
+                                        <a href="#" className="animate-navani delay-200 -m-3 flex items-center rounded-md p-3 text-sm font-base active:bg-violet-700">
+                                            <span className="ml-3 text-base font-medium text-white">About</span>
+                                        </a>
+                                        <a href="#" className="animate-navani delay-300 -m-3 flex items-center rounded-md p-3 text-sm font-base active:bg-violet-700">
+                                            <span className="ml-3 text-base font-medium text-white">Project</span>
+                                        </a>
+                                        <a href="#" className="animate-navani delay-400 -m-3 flex items-center rounded-md p-3 text-sm font-base active:bg-violet-700">
+                                            <span className="ml-3 text-base font-medium text-white">Contact</span>
+                                        </a>
                                     </nav>
                                 </div>
 
                                 <div className="grid items-center justify-start gap-4 mt-7">
-                                    <button className="cursor-pointer px-4" onClick={toggleMode}>
+                                    <button className="cursor-pointer px-4 animate-rightSideAni" onClick={toggleMode}>
                                         {isLightMode ? (
                                             <SunDim className="text-white" />
                                         ) : (
@@ -170,7 +143,7 @@ export default function Nav() {
                                     <button
                                         type="button"
                                         onClick={toggleLanguage}
-                                        className="rounded-[30px] ml-2 px-4 py-2 text-sm font-medium tracking-wider text-rose-50 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-75"
+                                        className="animate-rightSideAni rounded-[30px] ml-2 px-4 py-2 text-sm font-medium tracking-wider text-rose-50 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-75"
                                     >
                                         {isJapanese ? '日本語' : 'Eng'}
                                     </button>
@@ -181,5 +154,5 @@ export default function Nav() {
                 )}
             </div>
         </div>
-    )
+    );
 }
--- src/Home-Page/Dp-section/Dp.jsx
+++ src/Home-Page/Dp-section/Dp.jsx
@@ -1,51 +1,64 @@
 import React from "react";
-import { Linkedin } from 'lucide-react';
-import { Github } from 'lucide-react';
-import { Instagram } from 'lucide-react';
-import { Facebook } from 'lucide-react';
+import { FaLinkedin as Linkedin, FaGithub as Github, FaInstagram as Instagram, FaFacebook as Facebook } from 'react-icons/fa';
 
 import './dp.css';
 import dp from "../../assets/dp.png";
 
 export default function Dp() {
     return (
-        <>
-            <section className="w-[70%] mx-auto mt-10 mb-20 sm:mt-10">
-                <div className="grid grid-cols-[repeat(auto-fit,minmax(277px,1fr))] gap-y-8 items-center">
+        <section className="w-[70%] mx-auto mt-10 mb-20 sm:mt-10">
+            <div className="grid grid-cols-[repeat(auto-fit,minmax(277px,1fr))] gap-y-8 items-center">
+                <div>
+                    <h3 className="text-[1.5rem] text-white animate-topSideAni">Hi, It's Me</h3>
+                    <h1 className="text-[4rem] font-semibold text-white leading-[1] sm:py-2 py-3 animate-leftSideAni">I'm <span className="text-red-500">Pancham</span></h1>
+                    <p className="animate-topSideAni text-white text-[1.15rem] leading-7">
+                        A web developer passionate about crafting exceptional digital experiences. With expertise in both creative design and technical implementation, I specialize in delivering top-notch web solutions for clients worldwide.
+                        <br />
+                        Beyond coding, I'm fueled by my love for anime, manga, and dreams of coding in Japan. When I'm not at my desk, you can find me scaling mountains for inspiration.
+                    </p>
 
-                    <div>
-                        <h3 className="text-[1.5rem] text-white">Hi, It's Me</h3>
-                        <h1 className="text-[4rem] font-semibold text-white leading-[1] sm:py-2 py-3">I'm <span className="text-red-500">Pancham</span></h1>
-                        <p className="text-white text-[1.15rem] leading-7">
-                            A web developer passionate about crafting exceptional digital experiences. With expertise in both creative design and technical implementation, I specialize in delivering top-notch web solutions for clients worldwide.
-                            <br></br>
-                            Beyond coding, I'm fueled by my love for anime, manga, and dreams of coding in Japan. When I'm not at my desk, you can find me scaling mountains for inspiration.
-                        </p>
-
-                        <div className="flex flex-wrap gap-5 mt-7">
-                            <a href="https://www.linkedin.com/in/pancham-sardar-6a466227b/" target="_blank" rel="noopener noreferrer" className="shadow-2xl grid place-items-center text-center text-white text-lg border border-purple-800 rounded-full p-1.5 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-40 hover:bg-[#244D61] hover:shadow-md">
-                                <i><Linkedin /></i>
-                            </a>
-                            <a href="https://github.com/Panchammmm" target="_blank" rel="noopener noreferrer" className="shadow-2xl grid place-items-center text-center text-white text-lg border border-purple-800 rounded-full p-1.5 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-40 hover:bg-[#244D61] hover:shadow-md">
-                                <i><Github /></i>
-                            </a>
-                            <a href="https://www.instagram.com/pinaki_x_pancham/" target="_blank" rel="noopener noreferrer" className="shadow-2xl grid place-items-center text-center text-white text-lg border border-purple-800 rounded-full p-1.5 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-40 hover:bg-[#244D61] hover:shadow-md">
-                                <i><Instagram /></i>
-                            </a>
-                            <a href="https://www.facebook.com/your-profile-url" target="_blank" rel="noopener noreferrer" className="shadow-2xl grid place-items-center text-center text-white text-lg border border-purple-800 rounded-full p-1.5 transition duration-500 ease-in-out bg-gradient-to-r from-orange-500 to-purple-800 hover:opacity-40 hover:bg-[#244D61] hover:shadow-md">
-                                <i><Facebook /></i>
-                            </a>
-                        </div>
+                    <div className="flex flex-wrap gap-5 mt-7">
+                        <a
+                            href="https://www.linkedin.com/in/pancham-sardar-6a466227b/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="animate-SocialAni delay-100 shadow-2xl text-white text-lg border border-purple-800 rounded-full p-3 bg-gradient-to-r from-orange-400 to-purple-900 hover:shadow-md hover:bg-orange-200"
+                        >
+                            <Linkedin className="text-white" />
+                        </a>
+                        <a
+                            href="https://github.com/Panchammmm"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="animate-SocialAni delay-200 shadow-2xl text-white text-lg border border-purple-800 rounded-full p-3 bg-gradient-to-r from-orange-400 to-purple-900 hover:shadow-md hover:bg-orange-200"
+                        >
+                            <Github className="text-white" />
+                        </a>
+                        <a
+                            href="https://www.instagram.com/pinaki_x_pancham/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="animate-SocialAni delay-300 shadow-2xl text-white text-lg border border-purple-800 rounded-full p-3  bg-gradient-to-r from-orange-400 to-purple-900 hover:shadow-md hover:bg-orange-200"
+                        >
+                            <Instagram className="text-white" />
+                        </a>
+                        <a
+                            href="https://www.facebook.com/your-profile-url"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="animate-SocialAni delay-400 shadow-2xl text-white text-lg border border-purple-800 rounded-full p-3 bg-gradient-to-r from-orange-400 to-purple-900 hover:shadow-md hover:bg-orange-200"
+                        >
+                            <Facebook className="text-white" />
+                        </a>
                     </div>
 
-                    <div>
-                        <div className="images">
-                            <img src={dp} alt="dp" />
-                        </div>
+                </div>
+                <div>
+                    <div className="images">
+                        <img src={dp} alt="dp" className="animate-imageAni" />
                     </div>
                 </div>
-
-            </section>
-        </>
-    )
+            </div>
+        </section>
+    );
 }
