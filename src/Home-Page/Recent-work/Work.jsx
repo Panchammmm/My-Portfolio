95fea60f234ca2ca4ddf30234a1f1a53d8eeaf04
Panchammmm <sardarpancham510510@gmail.com>
2024-07-03 14:12:45 +0530

    tab-bar-animation

--- src/Home-Page/Recent-work/Work.jsx
+++ src/Home-Page/Recent-work/Work.jsx
@@ -11,6 +11,7 @@ export default function Work() {
         if (activeButton) {
             setLineStyle({
                 width: activeButton.offsetWidth,
+                height: activeButton.offsetHeight,
                 left: activeButton.offsetLeft
             });
         }
@@ -26,32 +27,29 @@ export default function Work() {
             <h1 className="my-work text-[45px] font-[700] text-center leading-[1.2]">My Recent Works</h1>
             <p className="tracking-[0.8px] text-white text-center mt-[15px]">Offering expert web design and development services, specializing in both frontend and backend solutions.</p>
 
-            <div className="tabbar-container">
+            <div className="mt-14 mx-auto">
                 <div className="tab_box">
-                    <button className={`tab_btn ${activeTab === 0 ? 'active' : ''}`} onClick={() => handleTabClick(0)}>ALL</button>
-                    <button className={`tab_btn ${activeTab === 1 ? 'active' : ''}`} onClick={() => handleTabClick(1)}>JLPT N5</button>
-                    <button className={`tab_btn ${activeTab === 2 ? 'active' : ''}`} onClick={() => handleTabClick(2)}>JLPT N4</button>
-                    <button className={`tab_btn ${activeTab === 3 ? 'active' : ''}`} onClick={() => handleTabClick(3)}>JLPT N3</button>
-                    <div className="line" style={lineStyle}></div>
+                    <button className={`tab_btn tab--1 ${activeTab === 0 ? 'active' : ''}`} onClick={() => handleTabClick(0)}>All</button>
+                    <button className={`tab_btn tab--2 ${activeTab === 1 ? 'active' : ''}`} onClick={() => handleTabClick(1)}>UI</button>
+                    <button className={`tab_btn tab--3 ${activeTab === 2 ? 'active' : ''}`} onClick={() => handleTabClick(2)}>Web&nbsp;App</button>
+                    <button className={`tab_btn tab--4 ${activeTab === 3 ? 'active' : ''}`} onClick={() => handleTabClick(3)}>Client</button>
+                    <div className="now-tab" style={lineStyle}></div>
                 </div>
 
-                <div className="content_box">
+                <div className="content_box bg-white mt-10">
+
                     <div className={`content ${activeTab === 0 ? 'active' : ''}`}>
-                        <h2>CRASH COURSES</h2>
-                        <p>A crash course in Japanese offers a rapid introduction to basic vocabulary, grammar, and conversation skills, providing a foundation for further learning.</p>
                     </div>
+
                     <div className={`content ${activeTab === 1 ? 'active' : ''}`}>
-                        <h2>JLPT N5</h2>
-                        <p>JLPT N5 is the introductory level of the Japanese Language Proficiency Test, assessing basic language skills in vocabulary, grammar, reading, and listening. To pass, focus on everyday vocabulary, essential grammar structures, and basic reading comprehension.</p>
                     </div>
+
                     <div className={`content ${activeTab === 2 ? 'active' : ''}`}>
-                        <h2>JLPT N4</h2>
-                        <p>The JLPT N4 tests your knowledge in understanding basic Japanese. This means that in addition to everything you learned in JLPT N5, you can: Understand about 1500 new vocabulary and about 170 new kanji.</p>
                     </div>
+
                     <div className={`content ${activeTab === 3 ? 'active' : ''}`}>
-                        <h2>JLPT N3</h2>
-                        <p>The JLPT N3 is the middle (intermediate) level of the Japanese Language Proficiency Test (JLPT). To pass the JLPT N3, you need to be able to read about 650 kanji and know about 3,750 vocabulary words.</p>
                     </div>
+
                 </div>
             </div>
         </section>
