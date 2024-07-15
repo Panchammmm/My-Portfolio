e28cd92374962f9ba89850d6d129f956e71ede1a
Panchammmm <sardarpancham510510@gmail.com>
2024-07-16 00:08:52 +0530

    home-section-responsive

--- src/Home-Page/Hero-Section/Dp.jsx
+++ src/Home-Page/Hero-Section/Dp.jsx
@@ -10,26 +10,25 @@ import insta from "../../assets/Social-media-icons/instagram.svg";
 export default function Dp() {
     return (
         <>
-            <section id="Home" className="lg:w-[85%] mx-auto lg:pt-8 lg:pb-20 lg:mb-14">
+            <section id="Home" className="lg:w-[85%] w-[90%] mx-auto lg:pt-8 lg:pb-20 lg:mb-14">
 
-                <div className="absolute top-1/2 left-1/2 transform -translate-x-[100%] -translate-y-[100%] -z-20">
-                    <img className="icon_say-hi" src={HiImage} alt="hi"></img>
+                <div className="absolute lg:top-1/2 lg:left-1/2 left-[52%] top-[20%] transform lg:-translate-x-[100%] lg:-translate-y-[100%] -translate-x-[0%] -translate-y-[100%] -z-20">
+                    <img className="icon_say-hi lg:w-[7rem] w-[5rem]" src={HiImage} alt="hi"></img>
                 </div>
 
                 <div className="grid grid-cols-[repeat(auto-fit,minmax(277px,1fr))] gap-y-8 items-center">
-
                     <div>
                         <div>
-                            <h3 className="text-[36px] text-white font-[700] mb-2 mt-14">I am Pancham</h3>
-                            <h1 className="text-[65px] font-[700] gradient-text leading-[1.2] mb-3">Web Developer + <br></br> UI Designer</h1>
+                            <h3 className="lg:text-[36px] text-[25px] text-white font-[700] mb-2  lg:mt-14 mt-8">I am Pancham</h3>
+                            <h1 className="lg:text-[65px] text-[40px] font-[700] gradient-text leading-[1.2] mb-3">Web Developer + <br></br> UI Designer</h1>
                         </div>
 
                         <p className="text-white text-[20px] leading-7 tracking-[1px] max-w-[550px] font-[300]">
                             Passionate about crafting <span className=" text-[#ffffff55]">exceptional</span> digital experiences. With expertise in both creative <span className=" text-[#ffffff55]">design</span> and <span className=" text-[#ffffff55]">technical</span> implementation, specialized in delivering top-notch <span className=" text-[#ffffff55]">web solutions</span> for clients worldwide.
                         </p>
 
-                        <div className="flex flex-wrap my-auto mt-10">
-                            <button className="flex tracking-[0.8px] border border-[#bb5dfed7] hover:bg-[#bb5dfed7] text-[#bb5dfed7] font-bold mr-8 py-3 px-7 rounded-full hover:text-white transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce gradient-border">
+                        <div className="flex flex-wrap my-auto lg:mt-10 mt-8">
+                            <button className="flex tracking-[0.8px] border border-[#bb5dfed7] hover:bg-[#bb5dfed7] text-[#bb5dfed7] font-bold mr-8 lg:py-3 lg:px-7 py-2 px-5 rounded-full hover:text-white transform transition-all duration-500 ease-in-out hover:scale-110 hover:brightness-110 hover:animate-pulse active:animate-bounce gradient-border">
                                 Resume
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
@@ -37,7 +36,7 @@ export default function Dp() {
                                     viewBox="0 0 24 24"
                                     strokeWidth="2"
                                     stroke="currentColor"
-                                    className="w-5 h-5 animate-bounce ml-1 mt-1"
+                                    className="w-4 h-4 lg:w-5 lg:h-5 animate-bounce ml-1 mt-1"
                                 >
                                     <path
                                         strokeLinecap="round"
@@ -47,12 +46,12 @@ export default function Dp() {
                                 </svg>
                             </button>
 
-                            <div className="flex ml-3 gap-5 my-auto text-[21px]">
+                            <div className="flex lg:ml-3 lg:gap-5 gap-3 my-auto">
                                 <a
                                     href="https://www.linkedin.com/in/pancham-sardar-6a466227b/"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="text-white hover:text-[#bb5dfed7] border border-[#bb5dfed7] size-10 p-[0.7rem] flex items-center justify-center rounded-full bg-gradient-to-r from-[#bb5dfed7] hover:from-transparent to-[#2a1454] transition hover:duration-700"
+                                    className="text-white hover:text-[#bb5dfed7] border border-[#bb5dfed7] lg:size-10 size-9 p-[0.7rem] flex items-center justify-center rounded-full bg-gradient-to-r from-[#bb5dfed7] hover:from-transparent to-[#2a1454] transition hover:duration-700"
                                 >
                                     <img src={linkedin} alt="linkedin"></img>
                                 </a>
@@ -61,7 +60,7 @@ export default function Dp() {
                                     href="https://github.com/Panchammmm"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="text-white hover:text-[#bb5dfed7] border border-[#bb5dfed7] size-10 p-[0.5rem] flex items-center justify-center rounded-full bg-gradient-to-r from-[#bb5dfed7] hover:from-transparent to-[#2a1454] transition hover:duration-700"
+                                    className="text-white hover:text-[#bb5dfed7] border border-[#bb5dfed7] lg:size-10 size-9 p-[0.5rem] flex items-center justify-center rounded-full bg-gradient-to-r from-[#bb5dfed7] hover:from-transparent to-[#2a1454] transition hover:duration-700"
                                 >
                                     <img src={github} alt="github"></img>
                                 </a>
@@ -70,7 +69,7 @@ export default function Dp() {
                                     href="https://www.instagram.com/pinaki_x_pancham/"
                                     target="_blank"
                                     rel="noopener noreferrer"
-                                    className="text-white hover:text-[#bb5dfed7] border border-[#bb5dfed7] size-10 p-[0.5rem] flex items-center justify-center rounded-full bg-gradient-to-r from-[#bb5dfed7] hover:from-transparent to-[#2a1454] transition hover:duration-700"
+                                    className="text-white hover:text-[#bb5dfed7] border border-[#bb5dfed7] lg:size-10 size-9 p-[0.5rem] flex items-center justify-center rounded-full bg-gradient-to-r from-[#bb5dfed7] hover:from-transparent to-[#2a1454] transition hover:duration-700"
                                 >
                                     <img src={insta} alt="insta"></img>
                                 </a>
@@ -79,7 +78,7 @@ export default function Dp() {
                     </div>
 
                     <div>
-                        <div className="images mx-auto">
+                        <div className="images mx-auto lg:w-[60%] w-[75%]">
                             <img src={dp} alt="dp" className="dp-img" />
                         </div>
                     </div>
