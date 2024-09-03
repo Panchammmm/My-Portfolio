42fe74f98bb352088204f184b5466045c85f8532
Panchammmm <sardarpancham510510@gmail.com>
2024-09-03 21:02:47 +0530

    update

--- src/Home-Page/About-section/About.jsx
+++ src/Home-Page/About-section/About.jsx
@@ -5,7 +5,7 @@ import Exp from "./Experience/Exp";
 export default function About() {
     return (
         <div className="pt-[120px] pb-[80px]">
-            <div className="flex flex-wrap justify-center gap-20 mx-6">
+            <div className="flex flex-wrap justify-center gap-24 mx-6">
                 <Education />
                 <Exp />
             </div>
